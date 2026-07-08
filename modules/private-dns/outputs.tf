5d8e43b83192b8c62c65d9977ec50766e89577bc
fyls237 <loicsteve27@gmail.com>
2026-07-08 23:43:41 +0000

    feat(infra): update modules name and remove tf lock

--- modules/private-dns/outputs.tf
+++ modules/private-dns/outputs.tf
@@ -4,10 +4,10 @@
 
 output "zone_ids" {
   description = "Map of DNS zone names to their resource IDs."
-  value       = { for k, v in azurerm_private_dns_zone.this : k => v.id }
+  value       = { for k, v in azurerm_private_dns_zone.private_dns_zone : k => v.id }
 }
 
 output "zone_names" {
   description = "List of Private DNS zone names created."
-  value       = [for v in azurerm_private_dns_zone.this : v.name]
+  value       = [for v in azurerm_private_dns_zone.private_dns_zone : v.name]
 }
