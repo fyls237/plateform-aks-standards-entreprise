db2381cd9623cb4e21d7f2329c56758886a3cd67
fyls237 <98533331+fyls237@users.noreply.github.com>
2026-08-18 00:47:22 +0200

    feat(test): deploy dev and pre-prod and prod environments with fixes (#34)
    * test(deploy): deploy dev environment, fix zone and var sub and add upload artefact
    
    * feat(test): deployement done for preprod dev prod

--- environments/test/backend.tf
+++ environments/test/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraform-state"
-    storage_account_name = "stterraformstate"
+    storage_account_name = "stterraformstateeb0206c9"
     container_name       = "tfstate"
     key                  = "test/platform-aks.tfstate"
   }
